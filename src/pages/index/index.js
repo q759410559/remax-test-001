1f8d7421c5ba725b044c1d5df136807605c38786
lwking <759410559@qq.com>
2021-04-22 20:05:46 +0800

    更新代码

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,60 +1,21 @@
 import * as React from 'react';
-import { View, Text, Image } from 'remax/wechat';
-import VanGrid from '@vant/weapp/dist/grid'
-import VanGridItem from '@vant/weapp/dist/grid-item'
-import styles from './index.css';
+import {View} from 'remax/wechat';
 
 export default () => {
+  function handleFooClick(event) {
+    console.log('bar')
+  }
+  function handleBarClick() {
+    console.log('foo')
+  }
   return (
       <View>
-        <VanGrid>
-          <VanGridItem icon="photo-o" text="文字" />
-          <VanGridItem icon="photo-o" text="文字" />
-          <VanGridItem icon="photo-o" text="文字" />
-          <VanGridItem icon="photo-o" text="文字" />
-        </VanGrid>
-
-        <VanGrid>
-          <VanGridItem icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
-          <VanGridItem icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
-          <VanGridItem icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
-          <VanGridItem icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
-        </VanGrid>
-
-        <VanGrid>
-          <VanGridItem text="文字">
-            <View slot="icon">
-              <Image
-                  style="width: 48px; height: 48px;"
-                  src="https://img.yzcdn.cn/vant/apple-1.jpg"
-              />
-            </View>
-          </VanGridItem>
-          <VanGridItem text="文字">
-            <View slot="icon">
-              <Image
-                  style="width: 48px; height: 48px;"
-                  src="https://img.yzcdn.cn/vant/apple-1.jpg"
-              />
-            </View>
-          </VanGridItem>
-          <VanGridItem text="文字">
-            <View slot="icon">
-              <Image
-                  style="width: 48px; height: 48px;"
-                  src="https://img.yzcdn.cn/vant/apple-1.jpg"
-              />
-            </View>
-          </VanGridItem>
-          <VanGridItem text="文字">
-            <View slot="icon">
-              <Image
-                  style="width: 48px; height: 48px;"
-                  src="https://img.yzcdn.cn/vant/apple-1.jpg"
-              />
-            </View>
-          </VanGridItem>
-        </VanGrid>
+        <View onClick={handleBarClick} style={{padding: '16', background: '#f44'}}>
+          bar
+          <View onClick={handleFooClick}  style={{padding: '16', background: '#00c6ff'}}>
+            foo
+          </View>
+        </View>
       </View>
   );
 };
